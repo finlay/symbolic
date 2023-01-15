ea1849cb14fc08f41781134c7c139324cbc357d6
Finlay Thompson <finlay@dragonfly.co.nz>
2023-01-15 13:30:50 +1300

    Getting into the Brauer isomorphism

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Data.Text (Text)
+import qualified Data.Text.IO as Text
+
+
+data H = E | I | J | K deriving (Eq, Ord)
+toStr :: H -> Text
+toStr E = "\\1"
+toStr I = "\\i"
+toStr J = "\\j"
+toStr K = "\\k"
+basis :: [H]
+basis = [ E, I, J, K ]
+
 
 main :: IO ()
 main = do
-  putStrLn ""
+  let ls = [ toStr x <> " \\otimes " <> toStr y | x <- basis, y <- basis]
+  mapM_ Text.putStrLn  ls
