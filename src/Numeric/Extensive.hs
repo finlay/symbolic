f47e7c834f31b37324634a085f63a2887e22fee5
Finlay Thompson <finlay@dragonfly.co.nz>
2023-01-27 17:20:12 +1300

    Started work on the symbolic calculator

--- src/Numeric/Extensive.hs
+++ src/Numeric/Extensive.hs
@@ -1,12 +1,14 @@
-module Numeric.Extensive 
+module Numeric.Extensive
     ( module Numeric.Extensive.Core
     , module Numeric.Extensive.Print
     , module Numeric.Extensive.Inverse
     , module Numeric.Algebra
-    ) 
+    )
     where
 
 import Numeric.Algebra
+import Prelude hiding ((+), (-), (*), (^), negate, (>), (<), sum, fromInteger)
+
 import Numeric.Extensive.Core
 import Numeric.Extensive.Print
 import Numeric.Extensive.Inverse
---
+++ src/Numeric/Symbolic.hs
@@ -0,0 +1,48 @@
+module Numeric.Symbolic where
+
+import Numeric.Algebra hiding (zero)
+import Prelude hiding ((+), (-), (*), (^), negate, (>), (<), sum, fromInteger)
+import Data.List (intercalate)
+
+--import Numeric.Extensive
+
+
+-- A symbol represents a function, which is expected to be a real function of n
+-- variables
+data Function = F String Int
+
+-- An expression represents something you can build up from the atomic fuctions
+data Expression
+  = Atomic Function
+  | Mul [ Expression ]
+  | Add [ Expression ]
+
+instance Multiplicative Expression where
+  (*) (Mul xs) (Mul ys) = Mul (xs <> ys)
+  (*) (Mul xs) y = Mul (xs <> [y])
+  (*) x (Mul ys) = Mul ([x] <> ys)
+  (*) x y = Mul [x, y]
+
+instance Additive Expression where
+  (+) (Add xs) (Add ys) = Add (xs <> ys)
+  (+) (Add xs) y = Add (xs <> [y])
+  (+) x (Add ys) = Add ([x] <> ys)
+  (+) x y = Add [x, y]
+
+instance Show Expression where
+  show (Atomic (F str _)) = str
+  show (Mul xs) = intercalate ""   $ map showWithParen xs
+  show (Add xs) = intercalate " + " $ map show xs
+
+showWithParen :: Expression -> String
+showWithParen (Add xs) = "(" <> show (Add xs) <> ")"
+showWithParen m = show m
+
+expand :: Expression -> Expression
+expand = undefined
+
+
+f,g,h :: Expression
+f = Atomic (F "f" 1)
+g = Atomic (F "g" 1)
+h = Atomic (F "h" 1)
