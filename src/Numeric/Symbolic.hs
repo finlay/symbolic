a1bc8f44d727f9c28ab855ccb8ce67e951bd2d28
Finlay Thompson <finlay@dragonfly.co.nz>
2023-03-13 16:49:56 +1300

    small tweaks

--- src/Numeric/Symbolic.hs
+++ src/Numeric/Symbolic.hs
@@ -31,7 +31,7 @@ instance Additive Expression where
 
 instance Show Expression where
   show (Atomic (F str _)) = str
-  show (Mul xs) = intercalate ""   $ map showWithParen xs
+  show (Mul xs) = intercalate ""    $ map showWithParen xs
   show (Add xs) = intercalate " + " $ map show xs
 
 showWithParen :: Expression -> String
@@ -39,8 +39,17 @@ showWithParen (Add xs) = "(" <> show (Add xs) <> ")"
 showWithParen m = show m
 
 expand :: Expression -> Expression
-expand = undefined
+expand (Atomic f) = Atomic f
+expand (Add xs) = Add [ expand x | x <- xs ]
+expand (Mul (x:xs)) = Add [ Mul (z:zs) | z <- expand x, zs <- expand xs ]
+expand (Mul []) = Add []
 
+--xs :: [[Int]]
+--xs = [[1,2,3,4], [7,8], [10,11,12]]
+--
+--expand :: [[a]] -> [[a]]
+--expand (y:ys) = [ z:zs | z <- y, zs <- expand ys]
+--expand [] = [[]]
 
 f,g,h :: Expression
 f = Atomic (F "f" 1)
